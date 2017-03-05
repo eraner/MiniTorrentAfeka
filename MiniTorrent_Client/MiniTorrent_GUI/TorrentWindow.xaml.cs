e17a58ab277733208477bf69ee96cd6df96cc1a4
ohad <OhadCohen21@gmail.com>
2017-03-05 16:45:35 +0200

    TorrentWindow with DataGrid. + fix to DBHelper.

--- MiniTorrent_Client/MiniTorrent_GUI/TorrentWindow.xaml.cs
+++ MiniTorrent_Client/MiniTorrent_GUI/TorrentWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MiniTorrent_MediationServerContract;
+using Newtonsoft.Json;
 
 namespace MiniTorrent_GUI
 {
@@ -19,13 +21,45 @@ namespace MiniTorrent_GUI
     /// </summary>
     public partial class TorrentWindow : Window
     {
+        private MediationReference.MediationServerContractClient client;
         private ConnectionDetails connectionDetails;
+        private string localIP;
+        private List<FileDetails> availableFiles;
 
-        public TorrentWindow(ConnectionDetails details)
+        public TorrentWindow(MediationReference.MediationServerContractClient client, ConnectionDetails details, string localIP)
         {
             InitializeComponent();
 
+            this.client = client;
             connectionDetails = details;
+            this.localIP = localIP;
+
+            updateAvailableFiles();
+            CollectionViewSource itemCollectionViewSource;
+            itemCollectionViewSource = (CollectionViewSource)(FindResource("ItemCollectionViewSource"));
+            itemCollectionViewSource.Source = availableFiles;
+        }
+
+        private void updateAvailableFiles()
+        {
+            string serializedData = client.GetAvailableFiles();
+            availableFiles = JsonConvert.DeserializeObject<List<FileDetails>>(serializedData);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            JsonItems json = new JsonItems
+            {
+                Username = connectionDetails.Username,
+                Password = connectionDetails.Password,
+                Ip = localIP
+            };
+            bool succeded = client.SignOut(JsonConvert.SerializeObject(json));
+            if (!succeded)
+            {
+                MessageBox.Show("Failed to Sing Out properly.\nPlease check your connection.","Failure to close", MessageBoxButton.OK, MessageBoxImage.Error);
+                e.Cancel = true;
+            }
         }
     }
 }
