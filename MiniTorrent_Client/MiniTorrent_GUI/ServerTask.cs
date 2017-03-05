54cf97aec2a986b7af5a13860ae290b94acf44a8
eraner <eraner21@gmail.com>
2017-03-05 16:46:57 +0200

    adding server task

---
+++ MiniTorrent_Client/MiniTorrent_GUI/ServerTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace MiniTorrent_GUI
+{
+    class ServerTask
+    {
+        private Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        public ServerTask()
+        {
+            //serverSocket.Bind(new IPEndPoint(IPAddress.Any, ))
+        }
+    }
+
+}
