d4cb0bea05b11d3c902ebf7b7beddcd3eca379d9
ohad <OhadCohen21@gmail.com>
2017-03-07 22:06:35 +0200

    adding downloadingFileItem

---
+++ MiniTorrent_Client/MiniTorrent_GUI/DownloadingFileItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTorrent_GUI
+{
+    class DownloadingFileItem
+    {
+        public string Filename { get; set; }
+        public float Size { get; set; }
+        public DateTime StartedTime { get; set; }
+        public DateTime EndedTime { get; set; }
+        public float Percentage { get; set; }
+    }
+}
