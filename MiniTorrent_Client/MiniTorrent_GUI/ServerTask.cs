12e432f99376561c487a8ae42ba93fbb70cc028b
eraner <eraner21@gmail.com>
2017-03-07 18:49:48 +0200

    serverr

--- MiniTorrent_Client/MiniTorrent_GUI/ServerTask.cs
+++ MiniTorrent_Client/MiniTorrent_GUI/ServerTask.cs
@@ -17,52 +17,70 @@ namespace MiniTorrent_GUI
         private List<Socket> clientSockets = new List<Socket>();
         private Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-        public ServerTask(ConnectionDetails connDetails)
-        {
-            this.connDetails = connDetails;
-            serverSocket.Bind(new IPEndPoint(IPAddress.Any, connDetails.OutgoingTcpPort));
-            serverSocket.Listen(4);
-            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
-        }
 
-        // waiting for clients callback
-        private void AcceptCallback(IAsyncResult ar)
-        {
-            Socket socket = serverSocket.EndAccept(ar);
-            clientSockets.Add(socket);
-            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
-            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
-        }
 
-        // recieve data from client.
-        private void ReceiveCallback(IAsyncResult ar)
-        {
-            Socket socket = (Socket)ar.AsyncState;
-            int received = socket.EndReceive(ar);
-            byte[] dataBuffer = new byte[received];
-            Array.Copy(buffer, dataBuffer, received);
 
-            /*Message from client - FileDataContract json string.*/
-            string receivedMsg = Encoding.ASCII.GetString(dataBuffer);
-            FileDataContract fileDetails = (FileDataContract)JsonConvert.DeserializeObject(receivedMsg);
-            byte[] sourceFile = File.ReadAllBytes(connDetails.PublishedFilesSource + fileDetails.Filename);
 
-            int bufSize = fileDetails.EndByte - fileDetails.StartByte;
-            if(bufSize <= 0)
-                return;
-            byte[] data = new byte[bufSize];
-            Array.Copy(sourceFile, fileDetails.StartByte, data, 0, bufSize);
+
+
+
+
+
+
+
+
+
+
+
+        //public ServerTask(ConnectionDetails connDetails)
+        //{
+        //    this.connDetails = connDetails;
+        //    serverSocket.Bind(new IPEndPoint(IPAddress.Any, connDetails.OutgoingTcpPort));
+        //    serverSocket.Listen(4);
+        //    serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        //}
+
+        //// waiting for clients callback
+        //private void AcceptCallback(IAsyncResult ar)
+        //{
+        //    Socket socket = serverSocket.EndAccept(ar);
+        //    clientSockets.Add(socket);
+        //    socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
+        //    serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        //}
+
+        //// recieve data from client.
+        //private void ReceiveCallback(IAsyncResult ar)
+        //{
             
-            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
-        }
+        //    Socket socket = (Socket)ar.AsyncState;
+        //    int received = socket.EndReceive(ar);
+        //    byte[] dataBuffer = new byte[received];
+        //    Array.Copy(buffer, dataBuffer, received);
 
-        private void SendCallback(IAsyncResult ar)
-        {
-            Socket socket = (Socket)ar.AsyncState;
-            socket.EndSend(ar);
-            socket.Close();
-            clientSockets.Remove(socket);
-        }
+            
+
+        //    /*Message from client - FileDataContract json string.*/
+        //    string receivedMsg = Encoding.ASCII.GetString(dataBuffer);
+        //    FileDataContract fileDetails = (FileDataContract)JsonConvert.DeserializeObject(receivedMsg);
+        //    byte[] sourceFile = File.ReadAllBytes(connDetails.PublishedFilesSource + fileDetails.Filename);
+
+        //    int bufSize = fileDetails.EndByte - fileDetails.StartByte;
+        //    if(bufSize <= 0)
+        //        return;
+        //    byte[] data = new byte[bufSize];
+        //    Array.Copy(sourceFile, fileDetails.StartByte, data, 0, bufSize);
+            
+        //    socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+        //}
+
+        //private void SendCallback(IAsyncResult ar)
+        //{
+        //    Socket socket = (Socket)ar.AsyncState;
+        //    socket.EndSend(ar);
+        //    socket.Close();
+        //    clientSockets.Remove(socket);
+        //}
     }
 
 }
